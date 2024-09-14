7881a01718d78ee959d7954ffdd073de5c3b42c2
Goberman15 <akbarrmdhn94@gmail.com>
2024-09-14 23:50:44 +0700

    Install Deps and Initial Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,33 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+import remarkToc from 'remark-toc'
+import wikiLinkPlugin from 'remark-wiki-link';
+import { getPermalinks } from '@portaljs/remark-wiki-link'
+
+const permalinks = getPermalinks("./src/content", [/\.ts$/, /\.js$/]);
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), sitemap()],
+  markdown: {
+    remarkPlugins: [
+      [wikiLinkPlugin, {
+        permalinks,
+        aliasDivider: '|',
+        pageResolver: (pageName) => {
+          const permalink = permalinks.find(p => p === pageName || p.endsWith(pageName))
+          return [permalink]
+        },
+        hrefTemplate: (permalink) => {
+          console.log({ permalink })
+          return "/" + permalink.replace(/^index|src\/content\/|\/index$/, '')
+        }
+      }],
+      [remarkToc, {
+        maxDepth: 3
+      }]
+    ],
+  }
+});
